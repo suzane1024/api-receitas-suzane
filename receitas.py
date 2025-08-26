ad2efd1e0b84b65bb994dd4d74f04f55bc936050
Suzane <suzane.maria10@aluno.ifce.edu.br>
2025-08-26 20:11:50 -0300

    Create receitas.py

---
+++ receitas.py
@@ -0,0 +1,7 @@
+receitas = [
+
+{'nome': 'brownie',
+ 'ingredientes': ['3 ovos, '6 colheres de açucar', '1 xicara de cacau em pó', '1 pitada de sal', '1 colher (chá) de essência de baunilha (opcional)', '1 xícara de farinha de trigo', '1 xícara de manteiga derretida'],
+ 'utensilio': ['Tigela grande', 'Colher ou fouet', 'Forma retangular untada,' 'Forno pré-aquecido,']
+ 'modo de preparo': ['Misture manteiga e açúcar', 'Acrescente os ovos, mexendo bem.' 'Junte o chocolate,' a farinha e o sal. 'Coloque a baunilha,' 'se quiser. Despeje na forma e asse em forno a 180 °C por cerca de 30 minutos.']
+ },
