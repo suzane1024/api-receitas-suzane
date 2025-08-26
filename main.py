cf8ff44562e8a1865e1f607e880755421c55b69f
Suzane <suzane.maria10@aluno.ifce.edu.br>
2025-08-26 23:29:28 +0000

    novas recetas

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI(title='API da Suzane')
+
+@app.get("/")
+def hello():
+    return {"title": "api-receitas-suzane"}
+
+@app.get("/receitas/{receita}")
+def get_receita():
+    return {"message": "Hello World!"}
