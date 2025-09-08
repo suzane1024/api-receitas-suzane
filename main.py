caba1a2f3d0447cbcfade0250494076b0e75a9c0
Suzane <suzane.maria10@aluno.ifce.edu.br>
2025-09-08 12:01:00 +0000

    receitas api

--- main.py
+++ main.py
@@ -6,43 +6,27 @@ app = FastAPI(title='API da Suzane')
 
 class Receita(BaseModel):
     nome: str
-    ingredients: List [str]
+    ingredients: List[str]
     modo_de_preparo: str
 
 receitas: List[Receita] = []
-    
+
 @app.get("/")
 def hello():
     return {"title": "api-receitas-suzane"}
 
-@app.get("/receitas/{receita}")
-def get_receita():
-    return {"message": "Livro de Receitas!"}
-
 @app.get("/receitas/")
 def get_todas_receitas():
-    return receitas 
+    return receitas
 
 @app.post("/receitas", response_model=Receita, status_code=201)
-def criar_receita(dados: Receita): 
-    nova_receita = dados
-
-    receitas.append(nova_receita)
-
-    return nova_receita
+def criar_receita(dados: Receita):
+    receitas.append(dados)
+    return dados
 
 @app.get("/receitas/{nome_receita}")
-def get_receita_por_nome(nome_receita: str ):
-    for receitas in receitas:
-        if receitas.nome == nome_receita:
+def get_receita_por_nome(nome_receita: str):
+    for receita in receitas:
+        if receita.nome == nome_receita:
             return receita
-        
-    return {"receita não encontrada"}
-
-@app.post("/receitas")
-def create_receita(dados: Receita):
-    nova_receitas = dados 
-
-    receitas.append(nova_receita)
-
-    return nova_receita
+    return {"erro": "receita não encontrada"}
